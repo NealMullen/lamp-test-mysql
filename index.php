854790bdfa98d4e49b9b6585fe8a72a4f0f28e00
Neal M <neal@ashfalldesign.com>
2014-11-20 19:28:18 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<title>Basic LAMP Test</title>
+	</head>
+	<body>
+		<?php
+		try {
+		  $conn = new PDO('mysql:host=192.168.56.104;dbname=test', 'neal', 'zildjian');
+		  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		  echo "Connection successful";
+		  if(empty($_GET["page"])):
+		  	$page = 1;
+			else:
+				$page=$_GET["page"];
+			endif;
+
+			$sth = $conn->prepare('SELECT * FROM pages WHERE id = :id');
+			$sth->bindParam(':id', $page);
+			$sth->execute();
+			$row = $sth->fetch(PDO::FETCH_ASSOC);
+			 ?>
+
+			<h1><?php echo $row['title'];?></h1>
+			<p><?php echo $row['content'];?></p>
+
+	<?php 
+		} catch (PDOException $er) {
+		  echo "Error!: " . $er->getMessage();
+		}
+		?>
+	</body>
+</html>
